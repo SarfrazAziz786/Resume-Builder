99950986ea7391de37c606dc921aaa640490e5c0
SarfrazAziz786 <162900723+SarfrazAziz786@users.noreply.github.com>
2024-09-11 00:19:07 +0500

    add profile pic selector

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 var _a;
 (_a = document.getElementById("form")) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
     event.preventDefault();
+    var profilePicInput = document.getElementById('profilePic');
     var nameElement = document.getElementById('name');
     var emailElement = document.getElementById('email');
     var phoneElement = document.getElementById('phone');
+    var addressElement = document.getElementById('address');
     var educationElement = document.getElementById('edu');
     var experienceElement = document.getElementById('experience');
     var skillElement = document.getElementById('skill');
-    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillElement) {
+    if (profilePicInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillElement) {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
+        var address = addressElement.value;
         var edu = educationElement.value;
         var experience = experienceElement.value;
         var skill = skillElement.value;
+        var profilePicFile = (_a = profilePicInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePicUrl = profilePicFile ? URL.createObjectURL(profilePicFile) : '';
         // const resumeOutput = `
-        var resumeOutput = " <h2>Resume</h2>\n   <p> <strong> Name: </strong> ".concat(name_1, "</p>   \n    <p> <strong> Email: </strong> ").concat(email, "</p>   \n    <p> <strong> Phone: </strong> ").concat(phone, "</p>\n\n    <h3> Education </h3>\n    <p> ").concat(edu, "</p>\n\n    <h3> Experience </h3>\n    <p> ").concat(experience, "</p>\n\n    <h3> Skill </h3>\n    <p> ").concat(skill, "</p>\n    \n    ");
+        var resumeOutput = " <h2>Resume</h2>\n\n\n   ".concat(profilePicUrl ? "<img src = ".concat(profilePicUrl, " alt=\"Profile_Picture\" class=\"profilePic\">") : '', "\n\n\n\n   <p> <strong> Name: </strong> ").concat(name_1, "</p>   \n    <p> <strong> Email: </strong> ").concat(email, "</p>   \n    <p> <strong> Phone: </strong> ").concat(phone, "</p>\n    <p> <strong> address: </strong> ").concat(address, "</p>\n\n    <h3> Education </h3>\n    <p> ").concat(edu, "</p>\n\n    <h3> Experience </h3>\n    <p> ").concat(experience, "</p>\n\n    <h3> Skill </h3>\n    <p> ").concat(skill, "</p>\n    \n    ");
         var resumeOutputElement = document.getElementById('resumeOutput');
         if (resumeOutputElement) {
             resumeOutputElement.innerHTML = resumeOutput;
--- main.ts
+++ main.ts
@@ -1,32 +1,49 @@
 document.getElementById("form")?.addEventListener('submit', function (event){
     event.preventDefault(); 
 
+    let profilePicInput=document.getElementById('profilePic') as HTMLInputElement
+
     let nameElement=document.getElementById('name') as HTMLInputElement
     let emailElement=document.getElementById('email') as HTMLInputElement
     let phoneElement=document.getElementById('phone') as HTMLInputElement
+    let addressElement=document.getElementById('address') as HTMLInputElement
+
+    
     let educationElement=document.getElementById('edu') as HTMLInputElement
     let experienceElement=document.getElementById('experience') as HTMLInputElement
     let skillElement=document.getElementById('skill') as HTMLInputElement
 
-    if(nameElement && emailElement && phoneElement && educationElement && experienceElement && skillElement ){
+    if(profilePicInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillElement ){
         const name= nameElement.value;
         const email= emailElement.value;
         const phone= phoneElement.value;
+        const address= addressElement.value;
         const edu= educationElement.value;
         const experience= experienceElement.value;
         const skill= skillElement.value;
 
-    
 
+
+
+        const profilePicFile=profilePicInput.files?.[0]
+
+        const profilePicUrl = profilePicFile ?  URL.createObjectURL (profilePicFile) : '';
 
 
 
     // const resumeOutput = `
    
    const resumeOutput =` <h2>Resume</h2>
+
+
+   ${profilePicUrl ? `<img src = ${profilePicUrl} alt="Profile_Picture" class="profilePic">` : ''}
+
+
+
    <p> <strong> Name: </strong> ${name}</p>   
     <p> <strong> Email: </strong> ${email}</p>   
     <p> <strong> Phone: </strong> ${phone}</p>
+    <p> <strong> address: </strong> ${address}</p>
 
     <h3> Education </h3>
     <p> ${edu}</p>
@@ -48,24 +65,6 @@ document.getElementById("form")?.addEventListener('submit', function (event){
         
     }
     
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
