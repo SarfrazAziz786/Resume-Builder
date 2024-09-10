e4949ded46820d3a70eb29ef94112e5e7c59e3e3
SarfrazAziz786 <162900723+SarfrazAziz786@users.noreply.github.com>
2024-09-10 14:17:13 +0500

    change file name main.html to index.html

---
+++ main.js
@@ -0,0 +1,27 @@
+var _a;
+(_a = document.getElementById("form")) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var nameElement = document.getElementById('name');
+    var emailElement = document.getElementById('email');
+    var phoneElement = document.getElementById('phone');
+    var educationElement = document.getElementById('edu');
+    var experienceElement = document.getElementById('experience');
+    var skillElement = document.getElementById('skill');
+    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillElement) {
+        var name_1 = nameElement.value;
+        var email = nameElement.value;
+        var phone = nameElement.value;
+        var edu = nameElement.value;
+        var experience = nameElement.value;
+        var skill = nameElement.value;
+        // const resumeOutput = `
+        var resumeOutput = " <h2>Resume</h2>\n   <p> <strong> Name: </strong> ".concat(name_1, "</p>   \n    <p> <strong> Email: </strong> ").concat(email, "</p>   \n    <p> <strong> Phone: </strong> ").concat(phone, "</p>\n\n    <h3> Education </h3>\n    <p> ").concat(edu, "</p>\n\n    <h3> Experience </h3>\n    <p> ").concat(experience, "</p>\n\n    <h3> Skill </h3>\n    <p> ").concat(skill, "</p>\n    \n    ");
+        var resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        }
+        else {
+            console.error("the resume output element are missing");
+        }
+    }
+});
